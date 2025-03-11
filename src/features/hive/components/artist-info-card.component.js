e75dc9bc122e715d1717596dcfd149251f304ce8
Raul <raul.puscas86@gmail.com>
2025-03-11 20:57:14 +0200

    star_rating

--- src/features/hive/components/artist-info-card.component.js
+++ src/features/hive/components/artist-info-card.component.js
@@ -4,6 +4,8 @@ import { Text, StyleSheet } from "react-native";
 import { Card } from "react-native-paper";
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import { ThemeProvider } from "styled-components/native";
+import star from "../../../../assets/star";
+import { SvgXml } from "react-native-svg";
 
 const ArtistCard = styled(Card)`
   background-color: ${(props) => props.theme.colors.bg.primary};
@@ -29,6 +31,12 @@ const Info = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
+const Rating = styled.View`
+  flex-direction: row;
+  padding-top: ${(props) => props.theme.space[2]};
+  padding-bottom: ${(props) => props.theme.space[2]};
+`;
+
 export const ArtistInfoCard = ({ artist = {} }) => {
   const {
     name = "Some Artist",
@@ -40,11 +48,18 @@ export const ArtistInfoCard = ({ artist = {} }) => {
     isClosedTemporarily,
   } = artist;
 
+  const ratingArray = Array.from(new Array(Math.floor(rating)));
+
   return (
     <ArtistCard elevation={5}>
       <ArtistCardCover key={name} source={{ uri: photos[0] }} />
       <Info>
         <Title>{name}</Title>
+        <Rating>
+          {ratingArray.map(() => (
+            <SvgXml xml={star} width={20} height={20} />
+          ))}
+        </Rating>
         <Address>{address}</Address>
       </Info>
     </ArtistCard>
