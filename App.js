bd3f7255afe5d0c6d020515757ea79b956211d42
Raul <raul.puscas86@gmail.com>
2025-03-21 14:02:06 +0200

    favourites

--- App.js
+++ App.js
@@ -12,6 +12,8 @@ import { theme } from "./src/infrastructure/theme";
 import { ArtistsContextProvider } from "./src/services/hive/artists.context";
 import { LocationContextProvider } from "./src/services/location/location.context";
 import { AppNavigator } from "./src/infrastructure/navigation/app.navigator";
+import { FavouritesContextProvider } from "./src/services/favourites/favourites.context";
+import { Navigation } from "./src/infrastructure/navigation";
 
 export default function App() {
   const [oswaldLoaded] = useOswald({
@@ -29,11 +31,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <LocationContextProvider>
-          <ArtistsContextProvider>
-            <AppNavigator />
-          </ArtistsContextProvider>
-        </LocationContextProvider>
+        <FavouritesContextProvider>
+          <LocationContextProvider>
+            <ArtistsContextProvider>
+              <Navigation />
+            </ArtistsContextProvider>
+          </LocationContextProvider>
+        </FavouritesContextProvider>
       </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
