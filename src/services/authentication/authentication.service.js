162eabdabfeabe4cc914ebc3f362e4f04df282da
Raul <raul.puscas86@gmail.com>
2025-03-24 16:48:10 +0200

    authentication_login

---
+++ src/services/authentication/authentication.service.js
@@ -0,0 +1,6 @@
+/* eslint-disable prettier/prettier */
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+// export const loginRequest = (auth, email, password) =>
+export const loginRequest = (auth, email, password) =>
+  signInWithEmailAndPassword(auth, email, password);
